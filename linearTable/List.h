80711d9c1c8c9736f07aec3e116941a5b8e09fe7
iahjc <hanruigang@163.com>
2016-12-23 16:10:09 +0800

    linear table final version
    three hours zero seven minute
    two thousand and sixteen years twelve month twenty days

--- linearTable/List.h
+++ linearTable/List.h
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
-
-#ifndef _List_H
-
-#define _List_H 0
+#include <malloc.h>
 
 #define MAXSIZE 100
-typedef char ElementType
+typedef char ElementType;
+
+//给结构取一个别名List
+typedef struct  
+{
+	ElementType Data[MAXSIZE];
+	int Last;
+}List;
+
+//比如 访问下标为I的元素：L.Data[i]或者Ptrl->Data[i]
+
 
 //申请一块空间	
 List *MakeEmpty();
-//根据位序K，返回相应元素
-ElementType FindKth(int K,List L);
 //在线性表L中查找X的第一次出现位置。
 int Find(ElementType X,List *Ptrl);
 //在位序i前插入一个新元素
@@ -23,17 +27,5 @@ int Length(List L);
 //显示列表里面的数据
 void ShowStr(List *Ptrl);
 
-#endif
 
 
-//给结构取一个别名List
-typedef struct 
-{
-	ElementType Data[MAXSIZE];
-	int Last;
-}List;
-
-//声明list
-List L,*Ptrl;
-
-//比如 访问下标为I的元素：L.Data[i]或者Ptrl->Data[i]
